2430bf053d2ea6197419d1e89f7ffe931b10c835
Eliacim <eliacim@eliacim.com.br>
2020-08-21 15:49:30 -0500

    Done

--- Python/Electronic-Station/Unix-match-part-2.py
+++ Python/Electronic-Station/Unix-match-part-2.py
@@ -9,20 +9,20 @@ Doing this manually can be time-consuming. 'Matching' or patterns for searching
 files by a specific mask are just what you need for these sort of challenges.
 
 This mission will help you understand how this works.
-
 You need to find out if the given unix pattern matches the given filename.
 
-Let me show you what I mean by matching the filenames in the unix-shell.
-For example, * matches everything and *.txt matches all of the files that have
-txt extension. Here is a small table that shows symbols that can be used in
-patterns.
+Here is a small table that shows symbols that can be used in patterns.
 
+[seq]   matches any character in seq, for example [123] means any character -
+'1', '2' or '3'
 
-*	matches everything
-?	matches any single character
+[!seq]  matches any character not in seq, for example [!123] means any
+character except '1', '2' and '3'
+
+Note that the expression in one pair of square brackets responds only for 1
+character. So ('0123', '[!abc]123') == True, but ('00123', '[!abc]123') = False
+
 Input: Two arguments. Both are strings.
-
-
 Output: Bool.
 Precondition: 0 < len(strings) < 100
 '''
@@ -33,17 +33,18 @@ from re import match
 
 def unix_match(filename: str, pattern: str) -> bool:
     pattern = pattern.replace('.', '\\.').replace('?', '.').replace('*', '.*')
+    pattern = pattern.replace('[!', '[^').replace('[]', '[^.]')
+    pattern = pattern.replace('[^]', '\\[!\\]')
     return match(pattern, filename) is not None
 
 
 if __name__ == '__main__':
     print("Example:")
-    print(unix_match('log12.txt', 'log?2.txt'))
+    print(unix_match('somefile.txt', '*'))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    assert unix_match('somefile.txt', '*') is True
-    assert unix_match('other.exe', '*') is True
-    assert unix_match('my.exe', '*.txt') is False
-    assert unix_match('log1.txt', 'log?.txt') is True
-    assert unix_match('log12.txt', 'log?.txt') is False
-    assert unix_match('log12.txt', 'log??.txt') is True
+    assert unix_match('somefile.txt', 'somefile.txt') is True
+    assert unix_match('1name.txt', '[!abc]name.txt') is True
+    assert unix_match('log1.txt', 'log[!0].txt') is True
+    assert unix_match('log1.txt', 'log[1234567890].txt') is True
+    assert unix_match('log1.txt', 'log[!1].txt') is False
